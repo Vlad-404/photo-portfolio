3ff18ed69a073d2777fd30054411d91f331a46e8
Vlad-404 <vmijat21@gmail.com>
2020-12-03 14:34:54 +0000

    Added for loop for categories with models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class categories(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=False)
+    image_url = models.CharField(max_length=500, null=False, blank=False)
+    image_id = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import categories
 
 # Create your views here.
 def index(request):
+    category = categories.objects.all()
+    context = {
+        'category': category
+    }
     # base index.html view
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
