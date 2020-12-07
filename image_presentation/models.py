1c3dae4169b9bc78b5360a987f7db9ed40487050
Vlad-404 <vmijat21@gmail.com>
2020-12-07 15:35:11 +0000

    Modified model from image_presentation app and imported fixtures

--- image_presentation/models.py
+++ image_presentation/models.py
@@ -3,17 +3,18 @@ from django.db import models
 
 # Create your models here.
 class Images(models.Model):
-    title = models.CharField(max_length=30),
-    description = models.TextField(),
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL),
-    panorama = models.BooleanField(default=False),
-    color = models.BooleanField(default=False),
-    price = models.DecimalField(max_digits=6, decimal_places=2),
-    rating = models.DecimalField(max_length=6, decimal_places=2, null=False, blank=False),
-    imgurl = models.URLField(max_length=1024, null=True, blank=True),
-    imgid = models.CharField(max_length=126, null=True, blank=True),
-    notes = models.CharField(max_length=254, null=False, blank=False),
-    uploaded_by = models.CharField(max_length=50, null=False, blank=False),
+    title = models.CharField(max_length=30, default=None, null=False, blank=False)
+    description = models.TextField(default=None)
+    category = models.CharField(max_length=24, default=None, null=False, blank=False)
+    panorama = models.BooleanField(null=False, blank=False, default=False)
+    color = models.BooleanField(null=False, blank=False, default=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=None)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=None,)
+    imgurl = models.URLField(max_length=1024, null=False, blank=False, default=None,)
+    imgid = models.CharField(max_length=126, null=True, blank=True, default=None,)
+    image = models.ImageField(null=False, blank=False, default=None)
+    notes = models.TextField(default=None)
+    uploaded_by = models.CharField(max_length=30, default=None)
 
     def __str__(self):
         return self.title
