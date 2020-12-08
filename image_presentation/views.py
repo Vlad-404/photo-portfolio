f0bf59a4422f3d23a6032ff36e8b480afacc49a0
Vlad-404 <vmijat21@gmail.com>
2020-12-08 16:31:06 +0000

    Added footer to base.html and randomise logic for images to galleries.html

--- image_presentation/views.py
+++ image_presentation/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from .models import Images
 
@@ -6,10 +7,14 @@ from .models import Images
 def all_images(request):
     """ A view to show all of the images """
     images = Images.objects.all()
-    random_list = Images.objects.order_by('?')
+    random_list1 = Images.objects.order_by('?')
+    random_list2 = Images.objects.order_by('?')
+    random_list3 = Images.objects.order_by('?')
     context = {
         'images': images,
-        'random': random_list,
+        'random1': random_list1,
+        'random2': random_list2,
+        'random3': random_list3,
     }
 
     return render(request, 'gallery/gallery.html', context)
