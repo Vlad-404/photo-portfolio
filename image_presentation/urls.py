584db478872f791c36ed897f598795f7168e2dc0
Vlad-404 <vmijat21@gmail.com>
2020-12-10 15:43:41 +0000

    Added image-view template

--- image_presentation/urls.py
+++ image_presentation/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_images, name='all_images'),
+    path('image_view/', views.image_view, name='image_view'),
 ]
--- image_presentation/views.py
+++ image_presentation/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from .models import Images
 from home.models import SocialMedia
 
+""" Social media links for navbar """
+media_links = SocialMedia.objects.all()
+
 
 # Create your views here.
+# Galleries display
 def all_images(request):
-    """ Import social media links for navbar """
-    media_links = SocialMedia.objects.all()
-
     """ A view that randomises the images each time page is loaded """
     random_list = Images.objects.order_by('?')
     context = {
@@ -17,3 +18,13 @@ def all_images(request):
     }
 
     return render(request, 'gallery/gallery.html', context)
+
+
+# More info for selected image
+def image_view(request):
+    """ A view that shows more info on an image and purchasing options """
+    context = {
+        'page_title': 'More info'
+    }
+
+    return render(request, 'gallery/image-view.html', context)
