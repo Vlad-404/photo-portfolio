a045b5c93757b4de789c735bb5131e7e3fe91d04
Vlad-404 <vmijat21@gmail.com>
2020-12-12 16:30:27 +0000

    Added sorting buttons to gallery

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
 // when clicking on search in mobile sidenav, it compacts the mobile side nav
 function hideMobileNavBar() {
-            mobileNavBar = document.getElementById("mobile-sidenav");
-            mobileNavBar.classList.remove("show");
-        }
+        mobileNavBar = document.getElementById("mobile-sidenav");
+        mobileNavBar.classList.remove("show");
+}
+
+// Toggles visibility for image info sidebar
+function hideInfoSidebar() {
+    sidebar = document.getElementById("image-info-and-purchase");
+    sidebar.classList.toggle("d-none");
+}
