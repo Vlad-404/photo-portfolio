44cc90afe3b3f49e4d14bb7dd09078ffdac36bfd
Vlad-404 <vmijat21@gmail.com>
2020-12-18 18:09:58 +0000

    Static files added to AWS

--- photo_portfolio/settings.py
+++ photo_portfolio/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'image_presentation',
+
+    # other
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -160,3 +163,21 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# AWS bucket configuration
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'vlad404-photo-portfolio'
+    AWS_S3_REGION_NAME = 'eu-central-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    # DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    # MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
