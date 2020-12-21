cc27af526d68950934404fa3a22faa23a03035cc
Vlad-404 <vmijat21@gmail.com>
2020-12-21 19:39:10 +0000

    Added bag contexts and delivery logic

--- photo_portfolio/settings.py
+++ photo_portfolio/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -164,6 +165,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # AWS bucket configuration
 if 'USE_AWS' in os.environ:
