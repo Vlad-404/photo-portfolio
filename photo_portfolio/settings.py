f7452b15bfd717710a96f3a1a99da09fb32898da
Vlad-404 <vmijat21@gmail.com>
2020-12-21 16:43:34 +0000

    Added back button when viewing the image and fixed some of the links in anchors

--- photo_portfolio/settings.py
+++ photo_portfolio/settings.py
@@ -166,6 +166,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS bucket configuration
 if 'USE_AWS' in os.environ:
+    # Cache controll
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'vlad404-photo-portfolio'
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
