9306b126d57170afbfddb853b8e2ff9c9c7f6f22
Vlad-404 <vmijat21@gmail.com>
2020-12-28 14:24:26 +0000

    Added more toasts and modified the message container for them

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from home.models import SocialMedia, Categories
 from django.contrib import messages
 
@@ -23,16 +23,19 @@ def view_cart(request):
 def add_to_cart(request, image_id):
     """ Add a number of specific image to the cart """
 
-    image = Images.objects.get(pk=image_id)
+    image = get_object_or_404(Images, pk=image_id)
     quantity = int(request.POST.get('quantity'))
     # redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if image_id in list(cart.keys()):
         cart[image_id] += quantity
+        messages.success(request,
+                         f'Quantity for "{image.title}" image has been updated')
     else:
         cart[image_id] = quantity
-        messages.success(request, f'You have successfully added {image.title} image to your cart')
+        messages.success(request,
+                         f'You have successfully added "{image.title}" to your cart. Click the cart icon above to see your cart.')
 
     request.session['cart'] = cart
     return redirect('all_images')
@@ -42,13 +45,18 @@ def adjust_cart(request, image_id):
     """Adjust the quantity of the specific image added to cart"""
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
+    image = get_object_or_404(Images, pk=image_id)
 
     if quantity > 0:
         cart[image_id] = quantity
+        messages.success(request,
+                         f'Quantity for "{image.title}" image has been updated')
         if quantity > 5:
             quantity = 5
     else:
         cart.pop(image_id)
+        messages.success(request,
+                         f'You have removed "{image.title}" from your cart')
 
     request.session['cart'] = cart
 
@@ -58,11 +66,15 @@ def adjust_cart(request, image_id):
 def remove_from_cart(request, image_id):
     """ Removes the item from shopping cart """
     try:
+        image = get_object_or_404(Images, pk=image_id)
         cart = request.session.get('cart', {})
         cart.pop(image_id)
+        messages.success(request,
+                         f'You have removed "{image.title}" from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing image: {e}')
         return HttpResponse(status=500)
