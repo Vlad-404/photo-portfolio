ecced18ee2903a7437482d476a3181ce742a0dac
Vlad-404 <vmijat21@gmail.com>
2020-12-28 16:54:56 +0000

    Installed and registered crispy_forms in settings

--- photo_portfolio/settings.py
+++ photo_portfolio/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
     # other
     'storages',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'photo_portfolio.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -82,6 +85,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
