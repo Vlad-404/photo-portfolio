0640bd29bbdee168c8b49051882e57ca1e87afba
Vlad-404 <vmijat21@gmail.com>
2020-12-28 12:26:48 +0100

    Set the debug to true and added 127.0.0.1 to allowed hosts

--- photo_portfolio/settings.py
+++ photo_portfolio/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'TRUE'
 
-ALLOWED_HOSTS = ['localhost', 'vlad404-photo-portfolio.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'vlad404-photo-portfolio.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
