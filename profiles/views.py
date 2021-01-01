8a54ba4c54d4b1ad14961c6dce96de1e19d99db7
Vlad-404 <vmijat21@gmail.com>
2021-01-01 20:45:15 +0000

    Added view and template to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
+# Imports models from other apps
+from home.models import Categories, SocialMedia
 
-# Display the user's profile
+# Defines variables for navbars and footer
+media_links = SocialMedia.objects.all()
+all_categories = Categories.objects.all()
+
+
+# Displays the user's profile
 def profile(request):
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'page_title': 'Your Profile',
+        'media_links': media_links,
+        'categories': all_categories,
+    }
 
     return render(request, template, context)
