82b1aecf2c586a1df099460e9e4d336318a37c9d
Vlad-404 <vmijat21@gmail.com>
2021-01-02 14:17:11 +0000

    Added default info for user form

--- profiles/models.py
+++ profiles/models.py
@@ -18,23 +18,6 @@ class UserProfile(models.Model):
                                 null=True,
                                 blank=True,
                                 )
-    default_country = CountryField(
-                                blank_label='Country *',
-                                max_length=20,
-                                null=True,
-                                blank=True
-                                )
-    default_postcode = models.CharField(
-                                max_length=20,
-                                null=True,
-                                blank=True,
-                                default=''
-                                )
-    default_town_or_city = models.CharField(
-                                max_length=40,
-                                null=True,
-                                blank=True
-                                )
     default_street_address1 = models.CharField(
                                 max_length=80,
                                 null=True,
@@ -47,6 +30,23 @@ class UserProfile(models.Model):
                                 )
     default_county = models.CharField(
                                 max_length=80,
+                                null=True,
+                                blank=True
+                                )
+    default_town_or_city = models.CharField(
+                                max_length=40,
+                                null=True,
+                                blank=True
+                                )
+    default_postcode = models.CharField(
+                                max_length=20,
+                                null=True,
+                                blank=True,
+                                default=''
+                                )
+    default_country = CountryField(
+                                blank_label='Country',
+                                max_length=20,
                                 null=True,
                                 blank=True
                                 )
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-
-from .models import UserProfile
+from django.contrib import messages
 
 # Imports models from other apps
 from home.models import Categories, SocialMedia
+from .models import UserProfile
+from.forms import UserProfileForm
+
 
 # Defines variables for navbars and footer
 media_links = SocialMedia.objects.all()
@@ -13,13 +15,25 @@ all_categories = Categories.objects.all()
 # Displays the user's profile
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
-    template = 'profiles/profile.html'
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all(
+
+    )
+    template = 'profiles/profile.html'
     context = {
         'page_title': 'Your Profile',
         'media_links': media_links,
         'categories': all_categories,
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
